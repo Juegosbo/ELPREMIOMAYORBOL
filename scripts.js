26b157b91c9db8a476d0b236899c480ea1afa9b3
Juegosbo <juegosbo24@gmail.com>
2024-08-02 17:31:10 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -722,7 +722,7 @@ document.addEventListener('DOMContentLoaded', () => {
             break;
             case 'letraB':
             cells = [
-                false,  true, false, true, true,
+                true,  true, false, true, true,
                 true, false,  true, false,  true,
                 true, false, false,  false, true,
                 true, false,  true, false,  true,
@@ -733,9 +733,9 @@ document.addEventListener('DOMContentLoaded', () => {
             case 'letraN':
             cells = [
                 true,  true, true, true, true,
-                false, true,  false, false,  false,
-                false, false, false,  false, false,
-                false, false,  false, true,  false,
+                false, true,  true, true,  false,
+                false, false, false,  true, false,
+                false, false,  true, true,  false,
                 true,  true, true, true, true
             ];
             figureImageSrc = 'diagonals.png'; // Cambia a la ruta de tu imagen
