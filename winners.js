388235a994c893a17c6a97d45a9a9d209bbc13f3
Juegosbo <juegosbo24@gmail.com>
2024-08-15 16:51:35 -0400

    Update winners.js

--- winners.js
+++ winners.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const winnersList = document.getElementById('winnersList');
     const figureOptionsForm = document.getElementById('figureOptionsForm');
-    const totalBoards = 2300;
+    const totalBoards = 2100;
     let generatedNumbers = JSON.parse(localStorage.getItem('generatedNumbers')) || [];
 
     // Verificar si los elementos son seleccionados correctamente
